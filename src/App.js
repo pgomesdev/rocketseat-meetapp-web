d2e21f0854c7159c702fa2f8eb88504b16a4be21
pgomesdev <pgomesdev@gmail.com>
2019-07-27 14:53:08 -0300

    refactor: moved route configs to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
+import './config/reactotron';
 
 import Routes from './routes';
 
+const history = createBrowserHistory();
+
 function App() {
-  return <Routes />;
+  return (
+    <Router history={history}>
+      <Routes />
+    </Router>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
-
-import './config/reactotron';
 
 import App from './App';
 
-const history = createBrowserHistory();
-
-ReactDOM.render(
-  <Router history={history}>
-    <App />
-  </Router>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
