5c87e262838d8415f913d7dc046634d266f0abf3
pgomesdev <pgomesdev@gmail.com>
2019-07-28 09:43:08 -0300

    feat: persisting data in local storage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import history from './services/history';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './config/reactotron';
 
 import Routes from './routes';
-import { store } from './store';
+import { store, persistor } from './store';
+import history from './services/history';
 
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <GlobalStyle />
-        <Routes />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <GlobalStyle />
+          <Routes />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
