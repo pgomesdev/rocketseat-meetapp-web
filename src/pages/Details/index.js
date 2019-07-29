9ace90726bc04a711a1a766b866683096e86333f
pgomesdev <pgomesdev@gmail.com>
2019-07-28 22:19:37 -0300

    feat: meetup detail interface

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import { MdEdit, MdDeleteForever, MdEvent, MdPlace } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, EditButton, CancelButton } from './styles';
 
 export default function Details() {
-  return <h1>Details</h1>;
+  return (
+    <Container>
+      <header>
+        <h1>Meetup de React Native</h1>
+        <div>
+          <EditButton>
+            <MdEdit size={20} color="#fff" />
+            Editar
+          </EditButton>
+          <CancelButton>
+            <MdDeleteForever size={20} color="#fff" />
+            Cancelar
+          </CancelButton>
+        </div>
+      </header>
+      <img
+        src="https://www.nekonation.com/wp-content/uploads/2015/11/DJ-LineUp-November-2015-Purrth-WEB-940x300.jpg"
+        alt="Meetup"
+      />
+      <p>
+        O Meetup de React Native é um evento que reúne a comunidade de
+        desenvolvimento mobile utilizando React a fim de compartilhar
+        conhecimento. Todos são convidados. Caso queira participar como
+        palestrante do meetup envie um e-mail para organizacao@meetuprn.com.br.
+      </p>
+      <div>
+        <p>
+          <MdEvent size={20} color="#fff" />
+          24 de Junho, às 20h
+        </p>
+        <p>
+          <MdPlace size={20} color="#fff" />
+          Rua Guilherme Gembala, 260
+        </p>
+      </div>
+    </Container>
+  );
 }
---
+++ src/pages/Details/styles.js
@@ -0,0 +1,97 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 940px;
+  margin: 50px auto;
+
+  > header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    h1 {
+      color: #fff;
+    }
+
+    div {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+  }
+
+  img {
+    display: flex;
+    flex-shrink: 1;
+    max-width: 940;
+    height: 300px;
+    margin: 50px 0 25px;
+  }
+
+  > p {
+    font-size: 18px;
+    color: #fff;
+    margin-bottom: 30px;
+  }
+
+  > div {
+    display: flex;
+    align-items: center;
+
+    p {
+      font-size: 16px;
+      color: rgba(255, 255, 255, 0.6);
+
+      display: flex;
+      align-items: center;
+
+      & + p {
+        margin-left: 30px;
+      }
+
+      svg {
+        opacity: 0.6;
+        margin-right: 10px;
+      }
+    }
+  }
+`;
+
+export const EditButton = styled.button.attrs({
+  type: 'button',
+})`
+  color: #fff;
+  font-size: 16px;
+  font-weight: bold;
+  padding: 11px 20px;
+  border-radius: 4px;
+  background: #4dbaf9;
+  border: 0;
+  margin-right: 15px;
+
+  display: flex;
+  align-items: center;
+
+  svg {
+    margin-right: 10px;
+  }
+`;
+
+export const CancelButton = styled.button.attrs({
+  type: 'button',
+})`
+  color: #fff;
+  font-size: 16px;
+  font-weight: bold;
+  padding: 11px 20px;
+  border-radius: 4px;
+  background: #d44059;
+  border: 0;
+
+  display: flex;
+  align-items: center;
+
+  svg {
+    margin-right: 10px;
+  }
+`;
