3b0e0b8246ba1f68f0a8d4affebdb3374eb7fa99
pgomesdev <pgomesdev@gmail.com>
2019-08-08 06:24:21 -0300

    feat: profile page ui

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import { MdAddCircleOutline } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <h1>Profile</h1>;
+  return (
+    <Container>
+      <Form>
+        <Input name="fullname" placeholder="Digite seu nome completo" />
+        <Input name="username" placeholder="Digite seu usário" />
+        <Input type="email" name="email" placeholder="Digite seu e-mail" />
+        <hr />
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Digite sua senha atual"
+        />
+        <Input
+          type="password"
+          name="password"
+          placeholder="Digite sua nova senha"
+        />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirme sua nova senha"
+        />
+        <button type="button">
+          <MdAddCircleOutline size={20} color="#fff" />
+          Salvar perfil
+        </button>
+      </Form>
+    </Container>
+  );
 }
---
+++ src/pages/Profile/styles.js
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  max-width: 940px;
+  margin: 50px auto;
+
+  form {
+    display: flex;
+    flex-direction: column;
+
+    input {
+      height: 50px;
+      padding: 15px 20px;
+      margin-bottom: 10px;
+      border-radius: 4px;
+      border: 0;
+      background: rgba(0, 0, 0, 0.2);
+      color: rgba(255, 255, 255, 0.5);
+      transition: 0.2s border;
+
+      &:focus {
+        border: 1px solid #fff;
+      }
+    }
+
+    hr {
+      margin: 20px 0;
+      border: 0;
+      height: 1px;
+      background: rgba(255, 255, 255, 0.1);
+    }
+
+    button {
+      width: 162px;
+      margin-top: 10px;
+      padding: 8px 18px;
+      border-radius: 4px;
+      background: #f94d6a;
+      border: 0;
+      color: #fff;
+      font-weight: bold;
+      font-size: 16px;
+      transition: 0.2s background;
+
+      align-self: flex-end;
+      display: flex;
+      align-items: center;
+
+      &:hover {
+        background: ${darken(0.03, '#f94d6a')};
+      }
+
+      svg {
+        margin-right: 10px;
+      }
+    }
+  }
+`;
